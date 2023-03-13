ac3083a0320569ea78ebaf66072d14bf4b183acb
Mona Lisa <ballboss6183@gmail.com>
2023-03-14 00:57:34 +0700

    okk

--- homework 1.cpp
+++ homework 1.cpp
@@ -1,9 +1,45 @@
 #include <iostream>
 using namespace std;
- int main() {
-   cout << (4|1)+(3^2);
 
 
+ 
+
+  // answer = 6
+int main() {
+   string s = "A";
+s+="B";
+cout << s;
+
+ }
+  // answer = AB
+
+
+
+  int main() {
+    cout << (4&1)<<1;
+ }
+
+  // answer = 01
+
+int main() {
+ cout << ('C'&~1);
+ }
+
+  // answer = 66
+
+   int main() {
+ cout << (char) ('C'&~1);
+ }
+
+  // answer = B
+
+
+  int main() {
+  cout << (3<<2>>1) <<endl ;
+  cout << (3>>2>>1); 
  }
 
   // answer = 6
+  // answer = 0
+
+
