0442db55314cdccfcde0bb7ef17fdc1bfeb7b202
Mona Lisa <ballboss6183@gmail.com>
2023-03-14 01:00:33 +0700

    okk

--- homework 1.cpp
+++ homework 1.cpp
@@ -42,4 +42,30 @@ int main() {
   // answer = 6
   // answer = 0
 
+   int main() {
+char s[] = "AAAAAAAAAAA 69";
+char x[3];
+int y; 
+sscanf(s,"%s %d",x,&y);
+cout << x << y;
+
+ }
+
+  // answer = AAAAAAAAAAA69
+
+
+   int main() {
+char s[] = "42,45 69:72";
+int a,b,c,d; 
+sscanf(s,"%d %d %d %d",&a,&b,&c,&d);
+cout << a << b << c << d << "\n";
+sscanf(s,"%d,%d %d:%d",&a,&b,&c,&d);
+cout << a << b << c << d << "\n";
+ }
+
+  // answer = 420260
+              42456972
+  
+  
+
 
